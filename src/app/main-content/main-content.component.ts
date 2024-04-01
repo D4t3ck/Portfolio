17e8a62e83ffb9f6fa51ca990659921371a2990b
Daniel Sinteck <d.sint3ck@gmail.com>
2024-04-01 17:52:26 +0200

    ++ main-content components;

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { AtfComponent } from '../main-content/atf/atf.component';
+import { AboutMeComponent } from '../main-content/about-me/about-me.component';
+import { TechStackComponent } from '../main-content/tech-stack/tech-stack.component';
+import { MyWorkComponent } from '../main-content/my-work/my-work.component';
+import { ContactComponent } from '../main-content/contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [
+    AtfComponent,
+    AboutMeComponent,
+    TechStackComponent,
+    MyWorkComponent,
+    ContactComponent,
+  ],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  styleUrl: './main-content.component.scss',
 })
-export class MainContentComponent {
-
-}
+export class MainContentComponent {}
