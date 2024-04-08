148b06aac8d142650483dfdef75f561041c35a53
Daniel Sinteck <d.sint3ck@gmail.com>
2024-04-08 04:09:36 +0200

    ++ my-work, tech-stack, contact;

--- src/app/main-content/my-work/my-work.component.ts
+++ src/app/main-content/my-work/my-work.component.ts
@@ -1,21 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-work',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-work.component.html',
-  styleUrl: './my-work.component.scss'
+  styleUrl: './my-work.component.scss',
 })
 export class MyWorkComponent {
-  isOpen: boolean = false;
+  projects = [
+    {
+      name: 'Join',
+      technologies: 'JavaScript | HTML | CSS',
+      description:
+        'Task manager inspired by the Kanban System. Create and organize task using drag and drop functions, assign users and categories. This project was developed by group work.',
+      link: '#',
+      github: '#',
+      img: 'join.png',
+    },
+    {
+      name: 'Pokedex',
+      technologies: 'JavaScript | Rest-Api | HTML | CSS',
+      description:
+        'Based on the PokéAPI a simple libary that provides and catalogues pokémon information',
+      link: '#',
+      github: '#',
+      img: 'pokedex.png',
+    },
+    {
+      name: 'El Pollo Loco',
+      technologies: 'JavaScript | HTML | SCSS',
+      description:
+        'A simple Jump-and-Run game based on an object-oriented approach. Play Pepe Peligroso and defeat the dangerous Chicken swarm! ',
+      link: '#',
+      github: '#',
+      img: 'pollo_loco.png',
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  toggleMenu() {
-    this.isOpen = !this.isOpen;
+  openNewWindow(url: string) {
+    window.open(url, '_blank');
   }
 }
--- src/app/main-content/tech-stack/tech-stack.component.ts
+++ src/app/main-content/tech-stack/tech-stack.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tech-stack',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tech-stack.component.html',
-  styleUrl: './tech-stack.component.scss'
+  styleUrl: './tech-stack.component.scss',
 })
 export class TechStackComponent {
-
+  skills = [
+    'Angular',
+    'TypeScript',
+    'JavaScript',
+    'HTML',
+    'CSS/SCSS',
+    'GIT',
+    'Firebase',
+    'Scrum',
+    'Rest-Api',
+    'Material Design',
+  ];
+  skillImg = [
+    'angular.png',
+    'typescript.png',
+    'javascript.png',
+    'html.png',
+    'css.png',
+    'git.png',
+    'firebase.png',
+    'scrum.png',
+    'rest-api.png',
+    'material-design.png',
+  ];
+  emailstring = 'mailto:daniel.sinteck@web.de';
 }
