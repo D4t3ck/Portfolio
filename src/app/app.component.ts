37425bfa3490b354318ddf7d2bef7c59d0c5c851
Daniel Sinteck <d.sint3ck@gmail.com>
2024-04-19 19:24:44 +0200

    ++ aos animation;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,51 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import aos from 'aos';
+/* import 'aos/dist/aos.css'; */
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,HeaderComponent, RouterOutlet, FooterComponent, LegalNoticeComponent, PrivacyPolicyComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    RouterOutlet,
+    FooterComponent,
+    LegalNoticeComponent,
+    PrivacyPolicyComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Portfolio';
+
+  AOS = aos;
+  constructor() {
+    this.AOS.init();
+
+    // You can also pass an optional settings object
+    // below listed default settings
+    this.AOS.init({
+      // Global settings:
+      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+      initClassName: 'aos-init', // class applied after initialization
+      animatedClassName: 'aos-animate', // class applied on animation
+      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+      // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+      offset: 120, // offset (in px) from the original trigger point
+      delay: 200, // values from 0 to 3000, with step 50ms
+      duration: 400, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: false, // whether elements should animate out while scrolling past them
+      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    });
+  }
 }
